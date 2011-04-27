d2da5eb029a35b71c3d4a7efac9dc49407afe764
Ryan Lewis <c00lryguy@gmail.com>
2011-04-26 23:46:54 -0400

    tests

--- test/test_byte.rb
+++ test/test_byte.rb
@@ -4,18 +4,64 @@ require 'helper'
 class TestByte < MiniTest::Unit::TestCase
   def setup; end
 
-  def test_from_hex
-    byte = Ohbad::Byte.from_hex("61")
-    assertion_hash = {
-      :to_hex => "61",
-      :to_dec => "97",
-      :to_oct => "141",
-      :to_bin => "01100001",
-      :to_ascii => "a"
-    }
-
-    assertion_hash.each do |method, result|
+  def assert_the_byte(byte, hsh={})
+    hsh.each do |method, result|
       assert_equal byte.send(method), result
     end
+  end
+
+  def test_from_oct_to_all
+    byte = Ohbad::Byte.from_oct("141")
+    assert_the_byte(byte, 
+      :to_oct   => "141",
+      :to_hex   => "61",
+      :to_ascii => "a",
+      :to_bin   => "01100001",
+      :to_dec   => "97"
+    )
+  end
+
+  def test_from_hex_to_all
+    byte = Ohbad::Byte.from_hex("62")
+    assert_the_byte(byte, 
+      :to_oct   => "142",
+      :to_hex   => "62",
+      :to_ascii => "b",
+      :to_bin   => "01100010",
+      :to_dec   => "98"
+    )
+  end
+
+  def test_from_bin_to_all
+    byte = Ohbad::Byte.from_bin("1100011")
+    assert_the_byte(byte, 
+      :to_oct   => "143",
+      :to_hex   => "63",
+      :to_ascii => "c",
+      :to_bin   => "01100011",
+      :to_dec   => "99"
+    )
+  end
+  
+  def test_from_ascii_to_all
+    byte = Ohbad::Byte.from_ascii("d")
+    assert_the_byte(byte, 
+      :to_oct   => "144",
+      :to_hex   => "64",
+      :to_ascii => "d",
+      :to_bin   => "01100100",
+      :to_dec   => "100"
+    )
+  end
+  
+  def test_from_dec_to_all
+    byte = Ohbad::Byte.from_dec("101")
+    assert_the_byte(byte, 
+      :to_oct   => "145",
+      :to_hex   => "65",
+      :to_ascii => "e",
+      :to_bin   => "01100101",
+      :to_dec   => "101"
+    )
   end
 end
